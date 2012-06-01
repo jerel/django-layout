6922cec4ac1eeb59ae7d4d7644c369f53eec22d1
Greg Aker <greg@gregaker.net>
2012-05-31 23:52:36 -0500

    Re-ordering things in INSTALLED_APPS, and adding compressor in there as well.

--- project_name/settings.py
+++ project_name/settings.py
@@ -24,16 +24,17 @@ LANGUAGES = (
 SECRET_KEY = '{{ secret_key }}'
 
 INSTALLED_APPS = (
-    # '{{ project_name }}.apps.',
-
-    'south',
-
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'south',
+    'compressor',
+
+    # '{{ project_name }}.apps.',
 )
 
 #============================================================================
