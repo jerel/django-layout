8b91363d002ea08f2feb181dc51d02ee1fc2ae9f
Greg Aker <greg@gregaker.net>
2013-04-01 23:49:17 -0500

    change urls.defaults to urls for django 1.5

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url, include
+from django.conf.urls import patterns, url, include
 
 urlpatterns = patterns('',
     # url('', include('{{ project_name }}.apps.'))
